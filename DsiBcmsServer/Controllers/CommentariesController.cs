706d3aadc29e23161aa27c17de06776af4bf61e4
Greg Doud <gpdoud@gmail.com>
2020-05-07 11:17:54 -0400

    Fixed error not adding pointsAvailable when an evaluation is assigned.

--- DsiBcmsServer/Controllers/CommentariesController.cs
+++ DsiBcmsServer/Controllers/CommentariesController.cs
@@ -7,66 +7,59 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using DSI.BcmsServer.Models;
 
-namespace DSI.BcmsServer.Controllers
-{
+namespace DSI.BcmsServer.Controllers {
     [Route("dsi/[controller]")]
     [ApiController]
-    public class CommentariesController : ControllerBase
-    {
+    public class CommentariesController : ControllerBase {
         private readonly DsiBcmsContext _context;
 
-        public CommentariesController(DsiBcmsContext context)
-        {
+        public CommentariesController(DsiBcmsContext context) {
             _context = context;
         }
 
         // GET: api/Commentaries
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Commentary>>> GetCommentary()
-        {
+        public async Task<ActionResult<IEnumerable<Commentary>>> GetCommentary() {
             return await _context.Commentary.ToListAsync();
         }
 
         // GET: api/Commentaries/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Commentary>> GetCommentary(int id)
-        {
+        public async Task<ActionResult<Commentary>> GetCommentary(int id) {
             var commentary = await _context.Commentary.FindAsync(id);
             //commentary.LastAccessUser = await _context.Users.FindAsync(commentary.LastAccessUserId);
 
-            if (commentary == null)
-            {
+            if(commentary == null) {
                 return NotFound();
             }
 
             return commentary;
         }
 
+        // POST: dsi/Commentaries/Update/5
+        // To get around problem with Put and Delete
+        [HttpPost("update/{id}")]
+        public async Task<IActionResult> UpdateCommentary(int id, Commentary commentary) {
+            return await PutCommentary(id, commentary);
+        }
         // PUT: api/Commentaries/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see https://aka.ms/RazorPagesCRUD.
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutCommentary(int id, Commentary commentary)
-        {
-            if (id != commentary.Id)
-            {
+        public async Task<IActionResult> PutCommentary(int id, Commentary commentary) {
+            if(id != commentary.Id) {
                 return BadRequest();
             }
 
+            commentary.Updated = Utility.Date.EasternTimeNow;
             _context.Entry(commentary).State = EntityState.Modified;
 
-            try
-            {
+            try {
                 await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!CommentaryExists(id))
-                {
+            } catch(DbUpdateConcurrencyException) {
+                if(!CommentaryExists(id)) {
                     return NotFound();
-                }
-                else
-                {
+                } else {
                     throw;
                 }
             }
@@ -78,21 +71,25 @@ namespace DSI.BcmsServer.Controllers
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see https://aka.ms/RazorPagesCRUD.
         [HttpPost]
-        public async Task<ActionResult<Commentary>> PostCommentary(Commentary commentary)
-        {
+        public async Task<ActionResult<Commentary>> PostCommentary(Commentary commentary) {
+            commentary.Created = Utility.Date.EasternTimeNow;
             _context.Commentary.Add(commentary);
             await _context.SaveChangesAsync();
 
             return CreatedAtAction("GetCommentary", new { id = commentary.Id }, commentary);
         }
 
+        // POST: dsi/Commentaries/Delete/5
+        // To get around problem with Put and Delete
+        [HttpPost("delete/{id}")]
+        public async Task<ActionResult<Commentary>> RemoveCommentary(int id) {
+            return await DeleteCommentary(id);
+        }
         // DELETE: api/Commentaries/5
         [HttpDelete("{id}")]
-        public async Task<ActionResult<Commentary>> DeleteCommentary(int id)
-        {
+        public async Task<ActionResult<Commentary>> DeleteCommentary(int id) {
             var commentary = await _context.Commentary.FindAsync(id);
-            if (commentary == null)
-            {
+            if(commentary == null) {
                 return NotFound();
             }
 
@@ -102,31 +99,11 @@ namespace DSI.BcmsServer.Controllers
             return commentary;
         }
 
-        private bool CommentaryExists(int id)
-        {
+        private bool CommentaryExists(int id) {
             return _context.Commentary.Any(e => e.Id == id);
         }
 
 
-
-        // POST: dsi/Commentaries/Update/5
-        // To get around problem with Put and Delete
-        [HttpPost("update/{id}")]
-        public async Task<IActionResult> UpdateCommentary(int id, Commentary commentary){
-
-         return await PutCommentary(id, commentary);
-
-        }
-
-        // POST: dsi/Commentaries/Delete/5
-        // To get around problem with Put and Delete
-        [HttpPost("delete/{id}")]
-        public async Task<ActionResult<Commentary>> RemoveCommentary(int id)
-        {
-
-            return await DeleteCommentary(id);
-
-        }
 
     }
 }
