61f58d76fe924ce8743babbddd44be572c8f0136
Greg Doud <gpdoud@gmail.com>
2021-11-02 10:50:59 -0400

    Fixed student checking into an inactive cohort
    * Monroe retook the bootcamp but still existed as a student in an old, inactive cohort. The check-in would end up being done to the inactive cohort.
    * Fix is searching for the student's cohort but only active cohorts (UsersController.GetActiveCohortByStudent)

--- DsiBcmsServer/Models/System.cs
+++ DsiBcmsServer/Models/System.cs
@@ -7,11 +7,11 @@ namespace DSI.BcmsServer.Models {
 
     public static class System {
 
-        public static readonly int MajorVersion = 0;
-        public static readonly int MinorVersion = 9;
-        public static readonly int PatchVersion = 0;
-        public static readonly string StatusVersion = "BETA";
-        public static readonly string Branch = "help-from-feedback-#4";
+        public static readonly int MajorVersion = 1;
+        public static readonly int MinorVersion = 4;
+        public static readonly int PatchVersion = 8;
+        public static readonly string StatusVersion = "PROD";
+        public static readonly string Branch = "fix-student-enrolled-in-two-corhorts-one-inactive)";
         public static string Version {
             get {
                 return $"v{MajorVersion}.{MinorVersion}.{PatchVersion} [{StatusVersion}] ({Branch}) ";
